f760b322fe6886d6a1a701ac2520f67356d48130
Aaryan-J <aaryanjoshi217@gmail.com>
2026-08-24 17:24:37 +0530

    add: README file add: welcome screen add: time string that shows local time and refreshes every second add: ability to drag around the welcome screen

---
+++ README.md
@@ -0,0 +1,16 @@
+# PeronsaOS
+__PersonaOS__ is a web-based OS ispired from the Persona 3 game. It's in the early stages of development right now and will be completed and shipped over time.
+
+### Features
+__PersonaOS__ currently has only a draggable window and a cool watch that tells the current time but not to worry as more stuff will be added soon! 
+Look below for planned features!
+
+### Future Plans
+Currently __PersonaOS__ is in development but these features will be implemented:
+
+1. 3 apps (for now)
+    - Music [This will link to my favourite artists and their albums]
+    - Books [This will link to my favourite authors and books]
+    - Media [This will link to my favourite shows, mostly anime, and movies]  
+2. A Persona 3 styled menu
+3. Cool animations
---
+++ main.js
@@ -0,0 +1,63 @@
+function updateTime() {
+  var currentTime = new Date().toLocaleString();
+  var timeString = document.querySelector("#dateAndTimeElement");
+  if (timeString) {
+    timeString.innerHTML = currentTime;
+  }
+}
+
+// Update time instantly, then run every second
+updateTime();
+setInterval(updateTime, 1000);
+
+// Initialize dragging functionality
+dragElement(document.getElementById("welcome"));
+
+function dragElement(element) {
+  if (!element) return;
+
+  var initialX = 0, initialY = 0;
+  var currentX = 0, currentY = 0;
+
+  var header = document.getElementById(element.id + "header");
+  if (header) {
+    header.onmousedown = startDragging;
+  } else {
+    element.onmousedown = startDragging;
+  }
+
+  function startDragging(e) {
+    e = e || window.event;
+    e.preventDefault();
+
+    // Get mouse cursor position at start
+    initialX = e.clientX;
+    initialY = e.clientY;
+
+    document.onmouseup = stopDragging;
+    document.onmousemove = elementDrag; // Fixed function link
+  }
+
+  // Renamed this inner function from dragElement to elementDrag to fix the loop crash as it was wrong in the demo itself
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+
+    // Calculate new position
+    currentX = initialX - e.clientX;
+    currentY = initialY - e.clientY;
+    initialX = e.clientX;
+    initialY = e.clientY;
+    
+    element.style.transform = "none";
+
+    // Set element's new positions
+    element.style.top = (element.offsetTop - currentY) + "px";
+    element.style.left = (element.offsetLeft - currentX) + "px";
+  }
+
+  function stopDragging() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
